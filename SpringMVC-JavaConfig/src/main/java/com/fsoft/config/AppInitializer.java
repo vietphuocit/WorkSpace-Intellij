9271d13593a4a1a066648e2102686ea1bc128403
phuoctktkhg <phuoctran27072001@gmail.com>
2023-01-15 23:58:47 +0700

    Spring MVC + JPA + Hibernate

---
+++ SpringMVC-JavaConfig/src/main/java/com/fsoft/config/AppInitializer.java
@@ -0,0 +1,30 @@
+package com.fsoft.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Ramesh Fadatare
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                PersistenceJPAConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WebMvcConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{
+                "/"
+        };
+    }
+}
